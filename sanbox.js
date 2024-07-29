1be8d25a0936459d03950ee0e39568b9f0896fcd
Selmy Nshuti <nshutistyles76@gmail.com>
2024-07-29 21:38:40 +0200

    first commit

---
+++ sanbox.js
@@ -0,0 +1,70 @@
+// keywords
+let age = 25;
+let year =2024;
+console.log(age, year);
+
+age = 35;
+console.log(age);
+
+const points = 125;
+console.log(points);
+
+var score = 225;
+console.log(score);
+
+score = 325;
+console.log(score);
+
+// strings
+
+console.log('Hello, world');
+
+let email = 'snshuti@centrika.rw';
+console.log(email);
+
+//string concatenation
+
+let fisrtName = 'Selmy';
+let lastName = 'Nshuti';
+let FullName = fisrtName + ' ' + lastName;
+console.log(FullName);
+
+//getting characters
+
+console.log(FullName[6]);
+
+//string length
+
+console.log(FullName.length);
+
+//string methods
+
+console.log(FullName.toUpperCase());
+
+let result = FullName.toLowerCase();
+console.log(result, FullName);
+
+//argument
+
+let index = email.indexOf('@');
+console.log(index);
+
+let count = fisrtName.indexOf('y');
+console.log(count);
+
+//common string methods
+
+let eemail = 'snshuti@centrika.rw'
+let rresult = eemail.lastIndexOf('k');
+console.log(rresult);
+
+//slice
+
+let rersult = eemail.slice(1,7);
+console.log(rersult);
+
+//substr
+
+let reresult = eemail.substring(0,10);
+console.log(reresult);
+
